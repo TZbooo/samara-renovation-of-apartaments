0dddac0975d4f2c7d5e68c133c4f7b3ce39d5ae8
NetBiom <110700797+TZbooo@users.noreply.github.com>
2022-10-22 20:50:34 +0300

    add sitemap.xml

--- samara_renovation_of_apartaments/samara_renovation_of_apartaments/urls.py
+++ samara_renovation_of_apartaments/samara_renovation_of_apartaments/urls.py
@@ -14,12 +14,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.sitemaps.views import sitemap
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 from django.urls import include
 
+from .sitemaps import StaticViewSitemap
+
+
+sitemaps = {
+    'static': StaticViewSitemap,
+}
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
+         name='django.contrib.sitemaps.views.sitemap'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
